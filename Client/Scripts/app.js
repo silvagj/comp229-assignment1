71084174140f7aa2fcd7b4ee63b244552eb1969e
Silvana Gjini <silvanagjini@gmail.com>
2022-06-05 12:38:04 -0400

    Assignment 1 complete version

--- Client/Scripts/app.js
+++ Client/Scripts/app.js
@@ -6,26 +6,14 @@
  *    Date:  2-Jun-2022
  *
  */
-//IIFE
-(function () {
-    function Start() {
-        console.log("App Started!");
-        // use AJAX to read JSON file
-        // Step 1: Create a new XHR object
-        let XHR = new XMLHttpRequest();
-        // Step 2: Create the request
-        XHR.open("GET", "../data/data.json");
-        // Step 3: Send the request
-        XHR.send();
-        // Step 4: create an event listener/handler
-        XHR.addEventListener("readystatechange", function () {
-            if (XHR.readyState == 4 && XHR.status == 200) {
-                console.log("JSON Data:");
-                console.log("===========");
-                console.log(XHR.responseText);
-            }
-        });
-    }
-    window.addEventListener("load", Start);
-})();
+const button = document.getElementById('send');
+button?.addEventListener('click', function send() {
+    //capture form information
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let phone = document.getElementById('phone').value;
+    let message = document.getElementById('message').value;
+    //redirect to homepage
+    window.location.href = './home';
+});
 //# sourceMappingURL=app.js.map
--- Client/Scripts/app.ts
+++ Client/Scripts/app.ts
@@ -6,37 +6,14 @@
  * 
  */
 
-//IIFE
-(function()
-{
+    const button = document.getElementById('send');
 
-    function Start()
-    {
-        console.log("App Started!");
-
-        // use AJAX to read JSON file
-
-        // Step 1: Create a new XHR object
-        let XHR = new XMLHttpRequest();
-
-        // Step 2: Create the request
-        XHR.open("GET", "../data/data.json");
-
-        // Step 3: Send the request
-        XHR.send();
-
-        // Step 4: create an event listener/handler
-        XHR.addEventListener("readystatechange", function()
-        {
-            if(XHR.readyState == 4 && XHR.status == 200)
-            {
-                console.log("JSON Data:");
-                console.log("===========");
-                console.log(XHR.responseText);
-            }
-        });
-    }
-
-    window.addEventListener("load", Start);
-
-})();
+    button?.addEventListener('click', function send() {
+      //capture form information
+      let name =(document.getElementById('name') as HTMLInputElement).value
+      let email =(document.getElementById('email') as HTMLInputElement).value
+      let phone =(document.getElementById('phone') as HTMLInputElement).value
+      let message =(document.getElementById('message') as HTMLInputElement).value
+      //redirect to homepage
+      window.location.href = './home';
+    });
